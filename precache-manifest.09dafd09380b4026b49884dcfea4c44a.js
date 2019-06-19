039718f0e504913f556e4ec1bd4d9e403c7c41df
Swathi Shetty <swathi1454n@gmail.com>
2019-06-19 11:53:19 +0530

    Updates

---
+++ precache-manifest.09dafd09380b4026b49884dcfea4c44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fdf9f791d93fb0efd152c046cd4eed",
+    "url": "/pwa-poc/index.html"
+  },
+  {
+    "revision": "3ca556461b357c0244c8",
+    "url": "/pwa-poc/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pwa-poc/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3ca556461b357c0244c8",
+    "url": "/pwa-poc/static/js/main.1298ed04.chunk.js"
+  },
+  {
+    "revision": "7e85abafc3d161b1a637",
+    "url": "/pwa-poc/static/js/runtime~main.95f38d26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
