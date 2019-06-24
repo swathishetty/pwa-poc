044e9dfebab4c7a48bcdd09df6f22726f70fa48c
Swathi Shetty <swathi1454n@gmail.com>
2019-06-24 15:33:51 +0530

    contact details api integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ReactJS
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component {
+      constructor(props){
+        super(props);
+        this.state ={
+          contactList : []
+        }
+      }
+    
+      componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const contactList = res.data;
+        this.setState({ contactList });
+      })
+      }
 
-export default App;
+      render(){
+        return (
+          <div>
+          <center><h1>Contact List</h1></center>
+          {this.state.contactList.map((contact) => (
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{contact.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
+                <p class="card-text">{contact.company.catchPhrase}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        )
+      }
+     
+    }
+    export default App;
