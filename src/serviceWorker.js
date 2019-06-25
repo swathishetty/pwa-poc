b953d6729d5be5421cc846ae6a9586961ed5550a
Swathi Shetty <swathi1454n@gmail.com>
2019-06-25 14:15:03 +0530

    uncommenting the service worker changes

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -33,7 +33,7 @@ export function register(config) {
 
     window.addEventListener('load', () => {
      //commenting const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-     const swUrl = `${process.env.PUBLIC_URL}/sw-template.js`;
+     const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
      console.log(' swUrlswUrl ',swUrl);
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
