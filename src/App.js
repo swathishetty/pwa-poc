7750325f46dcc3fd7314b223c18af419a5c84617
Swathi Shetty <swathi1454n@gmail.com>
2019-06-27 10:16:48 +0530

    signature canvas changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-
+import { Modal } from 'react-bootstrap';
+import SignatureCanvas from 'react-signature-canvas';
+ 
     class App extends Component {
       constructor(props){
         super(props);
         this.state ={
-          contactList : []
+          contactList: [],
+          showModal: false,
+          trimmedDataURL: '',
         }
+        this.setModal = this.setModal.bind(this);
+        this.clear = this.clear.bind(this);
+        this.trim = this.trim.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+      sigPad= {}
+
+      clear = () => {
+        this.sigPad.clear()
       }
-    
+
+      trim = () => {
+        this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
+          .toDataURL('image/png')})
+          this.setState({showModal: false});
+      }
+      setModal(){
+        alert("hello");
+        console.log('this.state.showModal ',this.state.showModal);
+        this.setState({showModal: true});
+        alert('showModal '+this.state.showModal);
+        console.log('this.state.showModal ',this.state.showModal);
+      }
+      handleClose(){
+        this.setState({showModal: false});
+      }
       componentDidMount() {
         axios.get(`https://jsonplaceholder.typicode.com/users`)
       .then(res => {
@@ -18,6 +46,7 @@ import axios from 'axios';
       }
 
       render(){
+        let {trimmedDataURL} = this.state;
         return (
           <div>
           <center><h1>Contact List</h1></center>
@@ -30,8 +59,35 @@ import axios from 'axios';
               </div>
             </div>
           ))}
+          <Modal show={this.state.showModal} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Please Sign</Modal.Title>
+          </Modal.Header>
+            <Modal.Body>
+                <SignatureCanvas penColor='green'
+              canvasProps={{width: 500, height: 500, className: 'sigCanvas'}} 
+              ref={(ref) => { this.sigPad = ref }}/>
+            </Modal.Body>
+            <Modal.Footer>
+              <button onClick={this.clear}>
+                Clear
+              </button>
+              <button onClick={this.trim}>
+                Sign
+              </button>
+            </Modal.Footer>
+          </Modal>
+          <div onClick={this.setModal}>
+          {
+            trimmedDataURL.length > 0 ?
+            <span>
+            Signature
+            <img src={trimmedDataURL} /> 
+            </span>:
+            "Click to Sign"
+          }
+          </div>
         </div>
-
         )
       }
      
